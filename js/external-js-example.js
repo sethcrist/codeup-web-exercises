8452cd9c594c8b8101c166db9ff5c79eb6f3e8b9
Seth Crist <sethcrist50@gmail.com>
2023-10-13 12:02:27 -0500

    feat: completed the js w/ html lec

---
+++ js/external-js-example.js
@@ -0,0 +1,22 @@
+"use strict";
+
+console.log(5 + 4); //console log prints to the console only
+let studentName = "Seth";
+console.log(studentName);
+
+let favoriteColor = "Orange";
+console.log(favoriteColor);
+
+//Alert! - A pop up message to the user from JavaScript
+alert("Howdy from Codeup!");
+
+//we can concatenate in side of an alert
+alert("howdy from " + studentName);
+
+//confirm - a pop up message to the user from JavaScript, that can capture a boolean
+let confirmed = confirm(`we have lunch at 12:30 today?`);
+console.log(confirmed);
+
+//Prompt - a pop up message to the user from JavaScript that asks for a text value
+let userString = prompt("what is your favorite food?");
+console.log(`Users favorite food: ${userString}`);
