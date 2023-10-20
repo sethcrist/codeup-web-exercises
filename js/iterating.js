8d6483bb54c61856686b2bafa85822c784434cb3
Seth Crist <sethcrist50@gmail.com>
2023-10-20 15:22:15 -0500

    finished with arrays exercise

--- js/iterating.js
+++ js/iterating.js
@@ -5,6 +5,46 @@
 
 // -------------NOTES-------------
 // do we use let or const for arrays?
+//elements are what is inside an array
+// Declaring an array.
+//     let emptyArr = [];
+//     console.log(Array.isArray(emptyArr));
+//
+//     let cheeses = ["Cheddar", "String", "Wensleydale"]; // This arrays contains 3 elements.
+//
+//     let falsyValues = [0, "", false, undefined, null]; // We can collect different data types using arrays.
+//     //...However, this can lead to some unintended results with a looping/iterative structure
+//
+//     console.log(cheeses[1]) // this will print the second element in our cheese array.
+//     console.log(cheeses.indexOf("Cheddar")); // This will return and print 0
+//     console.log(cheeses[0].indexOf("Brie")); // This returns -1
+//     console.log(cheeses.indexOf("White Cheddar")); // This will return and print -1
+//     console.log(cheeses[cheeses.length - 1]);
+//
+//     console.log(cheeses.length)
+//
+//     // Let's create a loop to print all of our cheeses to the console!
+//     for (let i = 0; i < cheeses.length; i++) {
+//         console.log(cheeses[i]);
+//     }
+//
+//     for (let i = cheeses.length - 1; i >= 0; i--) {
+//         console.log(cheeses[i]);
+//     }
+//
+//     //For Each version
+//     cheeses.forEach(function (cheese) {
+//         console.log(cheese);
+//     });
+//
+//     cheeses.forEach(function (cheese, index) {
+//         console.log(`${index}: ${cheese}`);
+//     });
+//
+//     cheeses.forEach(function (cheese, index, array) {
+//         console.log(`${index}: ${cheese}`);
+//         console.log(array);
+//     });
 
     /**
      * TODO:
@@ -58,6 +98,19 @@ let names = ["seth", "john", "mark", "pat"];
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+function first(value) {
+    return value[0];
+}
 
+    console.log(first(names));
+    function second(value) {
+        return value[1];
+}
+    console.log(second(names));
+    function last(value) {
+        return value[value.length -1];
+}
+
+    console.log(last(names));
 
 })();
